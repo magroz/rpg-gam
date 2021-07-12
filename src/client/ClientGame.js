b62eca9187da531a7ca9cd96167750c25ae53014
Mikhail Grozov <m.grozov@it.ecommpay.com>
2021-07-12 10:00:18 +0300

    homework-2:  init

---
+++ src/client/ClientGame.js
@@ -0,0 +1,41 @@
+// import ClientEngine from './ClientEngine';
+// import ClientsWorld from './ClientsWorld';
+//
+// import sprites from '../configs/sprites';
+// import levelCfg from '../configs/world.json';
+//
+// class ClientGame {
+//   constructor(cfg) {
+//     Object.assign(this, { cfg });
+//
+//     this.engine = this.createEngine();
+//     this.map = this.createWorld();
+//     this.initEngine();
+//   }
+//
+//   createEngine() {
+//     return new ClientEngine(document.getElementById(this.cfg.tagId));
+//   }
+//
+//   createWorld() {
+//     return new ClientsWorld(this, this.engine, levelCfg);
+//   }
+//
+//   initEngine() {
+//     this.engine.loadSprites(sprites).then(() => {
+//       this.engine.on('render', (_, time) => {
+//         this.map.init();
+//       });
+//       this.engine.start();
+//     });
+//   }
+//
+//   static init(cfg) {
+//     if (!ClientGame.game) {
+//       ClientGame.game = new ClientGame(cfg);
+//       console.log('Game INIT');
+//     }
+//   }
+// }
+//
+// export default ClientGame;
