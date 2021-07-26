9145b1f77e16ecd7bbfe33992a6f58b8cba8a2b9
Mikhail Grozov <m.grozov@it.ecommpay.com>
2021-07-26 22:36:28 +0300

    homework-5: 2 done

--- src/client/ClientGame.js
+++ src/client/ClientGame.js
@@ -19,17 +19,22 @@ class ClientGame {
   }
 
   createEngine() {
-    return new ClientEngine(document.getElementById(this.cfg.tagId));
+    return new ClientEngine(document.getElementById(this.cfg.tagId), this);
   }
 
   createWorld() {
     return new ClientsWorld(this, this.engine, levelCfg);
   }
 
+  getWorld() {
+    return this.world;
+  }
+
   initEngine() {
     this.engine.loadSprites(sprites).then(() => {
       this.world.init();
       this.engine.on('render', (_, time) => {
+        this.engine.camera.focusAtGameObject(this.player);
         this.world.render(time);
       });
       this.engine.start();
